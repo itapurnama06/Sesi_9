d4a13c02ab5d6185096df6badbaf5c0eceb86278
itapurnama06 <127809157+itapurnama06@users.noreply.github.com>
2023-05-09 12:12:34 +0800

    Add files via upload

---
+++ latihan01.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <button onclick="tulis()">Tampilkan text</button>
+
+    <div>
+        <form>
+            <div>
+                Nama Lengkap
+                <input type="text"name="txNAMA"id="txNama">
+                </div>
+                 <div>
+                    <button type="button">check Nama</button>
+                </div>
+
+        </form>
+    </div>
+
+    <script src="javaScript04.js"></script>
+   
+</body>
+</html>
